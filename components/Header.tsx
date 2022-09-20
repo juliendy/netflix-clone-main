652635d167c4b218b06e1c16c77317358cc706b2
juliendy <julien.yasar@gmail.com>
2022-09-20 11:10:24 +0200

    comments + fixes to headers in big screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import { SearchIcon, BellIcon } from '@heroicons/react/solid'
-import Link from 'next/link'
 import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 function Header() {
     const [isScrolled, setIsScrolled] = useState(false);
     
     useEffect(() => {
         const handleScroll = () => {
+          // if on top of the page (y-axis) show the header
             if (window.scrollY > 0) {
                 setIsScrolled(true)
             } else {
@@ -22,7 +23,10 @@ function Header() {
     },[])
 
   return (
+    // using effect with logical and operand to set color value
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      // space-x = space between in x-axis ie margin-left. 10=40px/2.5rem
+      // md: (medium) refers to responsiveness ie @media (min-width: 768px)
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
@@ -39,7 +43,7 @@ function Header() {
           <li className="headerLink">My List</li>
         </ul>
       </div>
-
+      // sm: (small) @media (min-width: 640px)
       <div className="flex items-center space-x-4 text-sm font-light">
         <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
