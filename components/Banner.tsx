62e5e253a49f8accac34911eba38af49d2b94de2
juliendy <julien.yasar@gmail.com>
2022-09-20 15:32:17 +0200

    rows with icons + some styling complete

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -25,12 +25,13 @@ function Banner({ netflixOriginals }: Props) {
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
-          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           objectFit="cover"
         />
       </div>
-      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-4xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
       <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,5 @@
-import { Movie } from "../typings"
-
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import { Movie } from '../typings'
 
 interface Props {
   title: string
@@ -7,12 +7,22 @@ interface Props {
 }
 
 function Row({ title, movies }: Props) {
-  return <div>
-    <h2>{title}</h2>
-    <div>
-        
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon className="absolute top-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+
+        <div>
+            {/* thumbnail goes here */}
+        </div>
+
+        <ChevronRightIcon className="absolute top-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+      </div> 
     </div>
-  </div>
+  )
 }
 
 export default Row
